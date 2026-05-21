ca5de2cd514ba773d869ccd095cacd098970aa06
Contrib <coder.github.robot@gmail.com>
2026-05-21 09:29:53 -0700

    feat: update realtime __init__ with event emitter exports

--- app/realtime/__init__.py
+++ app/realtime/__init__.py
@@ -1,4 +1,14 @@
 from app.realtime.manager import ConnectionManager, manager
 from app.realtime.channels import Channel
+from app.realtime.events import NotificationEvent, POSEvent, DashboardEvent
+from app.realtime.router import realtime_router
 
-__all__ = ["ConnectionManager", "manager", "Channel"]
+__all__ = [
+    "ConnectionManager",
+    "manager",
+    "Channel",
+    "NotificationEvent",
+    "POSEvent",
+    "DashboardEvent",
+    "realtime_router",
+]
